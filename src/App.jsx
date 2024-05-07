0472f06f71e78a01045beb8cb3340099a34dc2eb
Sweatha <89176350+Sweathadharan@users.noreply.github.com>
2024-05-07 17:23:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,21 @@ const imageData = ()=>{
 
 function App() {
   // code here
+  const images = imageData();
+  const imageElements = [];
+  for (let i = 0; i < images.length; i++) {
+    imageElements.push(
+      <img key={images[i].id} src={images[i].img} alt={images[i].id} />
+    );
+  }
+  return (
+    <div className="App">
+      <h1>Kalvium gallary</h1>
+      <div className="image-container">
+        {imageElements}
+      </div>
+    </div>
+  );
 }
 
 export default App;

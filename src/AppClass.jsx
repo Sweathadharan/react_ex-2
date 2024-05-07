34336a8b4a559c595aba567eead1b4b692d6efec
Sweatha <89176350+Sweathadharan@users.noreply.github.com>
2024-05-07 17:24:53 +0530

    Update AppClass.jsx

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -27,4 +27,26 @@ export default class AppClass extends Component{
   }
 
   // code here
+  render() {
+    
+    const images = this.imageData();
+
+    const imageElements = [];
+    for (let i = 0; i < images.length; i++) {
+      imageElements.push(
+        <img key={images[i].id} src={images[i].img} alt={`Image ${images[i].id}`} />
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Kalvium gallary</h1>
+        <div className="image-container">
+          {imageElements}
+        </div>
+      </div>
+    );
+  }
 }
+
+
